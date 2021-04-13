93b12a04cbbd6f599337ec00fe26ca462d44e9e5
Jesse Schulman <jesse@caleos.io>
2021-04-12 17:01:07 -0700

    Rename network

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 INSTALL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker network create -d bridge hyperion
+docker network create -d bridge docker_hyperion
 $INSTALL_ROOT/remove_eosio_docker.sh
 $INSTALL_ROOT/build_eosio_docker.sh
 $INSTALL_ROOT/start_eosio_docker.sh
