b030cec71628dfeb58cd55d922ff30717e3b1d84
Dewald928 <dewald123@rocketmail.com>
2021-09-20 11:09:03 +0200

    align evm with mainnet ram

--- scripts/init_blockchain.sh
+++ scripts/init_blockchain.sh
@@ -50,13 +50,13 @@ cleos wallet import -n eosio --private-key 5Jr65kdYmn33C3UabzhmWDm2PuqbRfPuDStts
 # deploy bios contract, this is required in getABI for system contracts 
 #deploy_contract.sh eosio.bios eosio eosio $(cat eosio_wallet_password.txt) true
 logInfo "creating eosio accounts"
-for i in eosio.token eosio.rex eosio.ram eosio.ramfee eosio.stake eosio.bpay eosio.vpay eosio.names eosio.saving eosio.trail; do
+for i in eosio.token eosio.rex eosio.ram eosio.ramfee eosio.stake eosio.bpay eosio.vpay eosio.names eosio.saving eosio.trail thisisatesta thisisatestb; do
   cleos create account eosio $i EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L #--stake-net "10.0000 TLOS" --stake-cpu "10.0000 TLOS" --buy-ram "100.0000 TLOS"
 done
 logInfo "deploying eosio.token"
 deploy_contract.sh eosio.token eosio.token eosio $(cat eosio_wallet_password.txt) true
 cleos push action eosio.token create '["eosio", "100000000000.0000 TLOS"]' -p eosio.token@active
-cleos push action eosio.token issue '[ "eosio", "1000000000.0000 TLOS", "memo" ]' -p eosio@active
+cleos push action eosio.token issue '[ "eosio", "350000000.0000 TLOS", "memo" ]' -p eosio@active # the actual amount of telos
 
 logInfo "activating"
 curl -X POST http://127.0.0.1:8888/v1/producer/schedule_protocol_feature_activations -d '{"protocol_features_to_activate": ["0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"]}'
@@ -70,6 +70,9 @@ cleos push action eosio init '[0, "4,TLOS"]' -p eosio@active
 
 #'{"threshold":1,"keys":[],"accounts":[{"permission":{"actor":"eosio","permission":"active"},"weight":1}]}'
 
+cleos push action eosio setram '[15285317824]' -p eosio@active # current mainnet amount of ram
+cleos push action eosio buyrambytes '{"payer":"eosio","receiver":"thisisatesta","bytes":4000000000}' -p eosio@active 
+cleos push action eosio buyrambytes '{"payer":"eosio","receiver":"thisisatestb","bytes":2500000000}' -p eosio@active
 
 logInfo "creating eosio.evm"
 cleos system newaccount eosio eosio.evm EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L  --stake-net "10.0000 TLOS" --stake-cpu "10.0000 TLOS" --buy-ram "10000.0000 TLOS"
@@ -82,6 +85,7 @@ cleos system newaccount eosio rpc.evm EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfw
 logInfo "creating evmuser1"
 cleos system newaccount eosio evmuser1 EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L EOS5GnobZ231eekYUJHGTcmy2qve1K23r5jSFQbMfwWTtPB7mFZ1L --stake-net "10.0000 TLOS" --stake-cpu "10.0000 TLOS" --buy-ram "10000.0000 TLOS"
 cleos transfer eosio evmuser1 "111000000.0000 TLOS"
+# cleos transfer eosio evmuser1 "11100000.0000 TLOS"
 
 logInfo "deploying eosio.evm"
 #deploy_contract.sh eosio.evm eosio.evm eosio $(cat eosio_wallet_password.txt) true
