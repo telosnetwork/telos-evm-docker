c42b5a93824de8d90cfc9582d6d393ff366eb013
Guillermo Rodriguez <guillermor@fing.edu.uy>
2021-12-17 10:35:34 -0300

    Added config verb to tevmc tool, allows to config hyperion to only index evm actions, also block number to start from and stop.
    Edited CI to use this new config to only index evm.

--- tevmc/tevmc.py
+++ tevmc/tevmc.py
@@ -520,6 +520,63 @@ def pull():
 
 @cli.command()
 @click.option(
+    '--hyperion-cfg-path', default='docker/hyperion/config/chains/telos-testnet.config.json',
+    help='Path to hyperion config file')
+@click.option(
+    '--indexer-start-on', default=1,
+    help='Block number at which hyperion indexer must start.')
+@click.option(
+    '--indexer-stop-on', default=0,
+    help='Block number at which hyperion indexer must stop.')
+@click.option(
+    '--evm-abi-path', default='docker/eosio/contracts/eosio.evm/eosio.evm.abi',
+    help='Path to eosio.evm abi file, to parse actions and support --index-only-evm')
+@click.option(
+    '--index-only-evm/--index-all', default=False,
+    help='Show output while waiting for bootstrap.')
+def config(
+    hyperion_cfg_path,
+    indexer_start_on,
+    indexer_stop_on,
+    evm_abi_path,
+    index_only_evm
+):
+
+    def decode_or_die(file):
+        try:
+            return json.loads(file.read())
+
+        except json.decoder.JSONDecodeError as e:
+            print('error parsing config! {e}')
+            sys.exit(1)
+
+    # hyperion
+    with open(hyperion_cfg_path) as config_file:
+        config = decode_or_die(config_file)
+
+    # index_only_evm
+    # hyperion has an indexer action whitelist, so to only index
+    # evm transaction we must add all eosio.evm actions to the 
+    # whitelist, to dynamically get all the actions we parse the
+    # provided abi file
+    actions = []
+    if index_only_evm:
+        with open(evm_abi_path, 'r') as abi_file:
+            abi = decode_or_die(abi_file)
+        actions = [row['name'] for row in abi['actions']]
+
+    config['whitelists']['actions'] = actions
+
+    config['indexer']['start_on'] = indexer_start_on
+    config['indexer']['stop_on'] = indexer_stop_on
+
+    # truncate config and re-write
+    with open(hyperion_cfg_path, 'w+') as config_file:
+        config_file.write(json.dumps(config, indent=4))
+
+
+@cli.command()
+@click.option(
     '--pid', default='/tmp/tevmc.pid',
     help='Path to lock file for daemon')
 @click.option(
