0c253b9c03dd931fca48a509345503d64c5183f5
Guillermo Rodriguez <guillermo@telos.net>
2022-07-07 20:00:34 -0300

    Pull py-eosio from host_deploy branch, thanks to tom for debug help

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'daemonize',
         'simplejson',
         'simple_rlp@git+https://github.com/guilledk/simple-rlp.git',
-        'py_eosio@git+https://github.com/guilledk/py-eosio.git'
+        'py_eosio@git+https://github.com/guilledk/py-eosio.git@host_deploy'
     ],
     include_package_data=True,
     entry_points={
